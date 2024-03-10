69232dd97d3d9659ab6fd37396d4b40d07296893
oboy21 <oboy11b@gmail.com>
2024-03-10 17:48:31 +0300

    Closures

--- starter/script.js
+++ starter/script.js
@@ -248,3 +248,22 @@ runOnce();
 }
 // console.log(isPrivate);
 console.log(notPrivate); */
+
+// ///////////////////////////////////////
+// // Closures
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
